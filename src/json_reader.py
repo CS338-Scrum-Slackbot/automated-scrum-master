7d759aaa529d0a101973c8f5044ba36eff756dc5
Caspar Popova <71401509+cjpopova@users.noreply.github.com>
2021-04-16 23:46:50 -0500

    Completed create, delete ops. Removed update op

--- src/json_reader.py
+++ src/json_reader.py
@@ -2,107 +2,96 @@ import json
 from os import read
 
 class json_interface():
-    def __init__(self, file_path: str = "../data/scrum_board.json"):
-        pass # Opens file_name in data directory. default file_name is scrum_board.json
-    def create(self, entry: object, log: str = "product_backlog"):
-        pass # Adds the entry to specified log in file_name
+    def __init__(self, file_path):
+        pass # Opens file_name in data directory
+    def create(self, entry: object, log: str):
+        pass # Adds the entry to specified log
     def read(self, id: int, log: str = None): # Return Tuple[object, str]
-        pass # Returns entry id# and log-str. If input log if None (default), must scan all logs.
+        pass # Returns entry obj and log str. If input log if None (default), must scan all logs.
              # If id# is not in log or file, returns None.
-    def update(self, id: int, entry: object, log: str = None): # Return Tuple[object, str]
-        pass # Replaces id# with new entry
-    def delete(self, id: int, log: str = None): # Return str
-        pass # Removes id# from log (!!HARD DELETE!!)
+    def delete(self, id: int, log: str = None): # Return Tuple[obj, str]
+        pass # Removes id# from log (!!HARD DELETE!!). Use for update operations.
     def search_string(self, lookup: str, log: str = None) -> object:
         pass # lookup by string
     def list_logs(self) -> list:
-        pass # Returns a list of logs in file_name
+        pass # Returns a list of logs in file_name: ["product_backlog","sprint_backlog",...]
     def close(self):
-        pass # closes the file reader
+        pass # Closes the file reader
 
-"""  TODO return which log the object is located in
-hopefully the json reader will not be used to manage any non scrum board files
-may need to implement search by status, priority, etc
+"""
+TODO: implement search by status, priority, etc
+may be coupled with id search
+repeated code in read, delete - can be abstracted if you have time
 """
 
 class json_reader(json_interface):
-    def __init__(self, file_path: str = "../data/scrum_board.json"):
-        self._f = open(file=file_path, mode="r+") # access mode read and write. TODO: slashes
+    def __init__(self, file_path: str):
+        self._f = open(file=file_path, mode="r+") # access mode: read and write. TODO: slashes
         self._j = json.load(self._f)
-        
-    # Helper function for reading specific log
-    def _read_log(self, id: int, r_log: str) -> object:
-        entries = self._j[r_log]
-        for e in entries:
-            if e["id"] == id:
-                return e
-        return None
     
     def create(self, entry: object, log: str):
-        pass # Adds the entry to specified log in file_name
+        self._j[log].append(entry)                   # Add to python obj
+        self._f.seek(0)
+        self._f.write(json.dumps(self._j, indent=4)) # Write python obj to file
+        self._f.truncate()
     
     def read(self, id: int, log: str = None): # Return Tuple[object, str]
-        if log != None:
+        # Helper function for reading specific log
+        def _read_log(self, r_log: str) -> object:
+            entries = self._j[r_log]
+            for e in entries:
+                if e["id"] == id:
+                    return e
+            return None
+        # Read specified log
+        if log is not None:
             return self._read_log(id, log), log
-        else: # scan over logs
-            for l in self.list_logs():
-                result = self._read_log(id, l)
-                if (result != None):
-                    return result, l
-            return None, None
+        # Scan over logs
+        for l in self.list_logs():
+            result = self._read_log(id, l)
+            if (result != None):
+                return result, l
+        return None, None
 
-    def update(self, id: int, entry: object, log: str = None): # Return Tuple[object, str]
-        # TODO: does not check if id# already exists somewhere
-        pass # idk if this should actually be an operation
-        """if log is None:
-            for l in self.list_logs():
-                result = self._read_log(id, l)
-                if (result != None):
-                    log = l
-        self._j[log].append(story)
-        print(self.sb['product_backlog'])
-        with open(SCRUM_BOARD, 'w') as f
-            json.dump(self.sb, f, indent=4)"""
-
-
-    def delete(self, id: int, log: str = None):
+    def delete(self, id: int, log: str = None): # Returns Tuple[object, str]
         # Helper function for deleting from specific log
         def delete_from_log(l: str):
             entries = self._j[l] # Array of objs
             for idx in range(0, len(entries)):
                 if entries[idx]["id"] == id:
-                    self._j[l].pop(idx) # Remove from python obj
+                    e = self._j[l].pop(idx) # Remove from python obj
                     self._f.seek(0)
                     self._f.write(json.dumps(self._j, indent=4)) # Write python obj to file
                     self._f.truncate()
-                    return l
-            return None
-
+                    return e, l
+            return None, None
+        # Remove from specified log
         if log is not None:
             return delete_from_log(log)
-
+        # Iterate over all logs if not specified
         log_list = self.list_logs()
         num_logs = len(log_list)
         idx = 0
         result = None
         while idx < num_logs and result is None:
-            result = delete_from_log(log_list[idx])
+            result, _ = delete_from_log(log_list[idx])
             idx = idx + 1
-        return result
-        
+        return result, log_list[idx-1]
 
     def search_string(self, lookup: str, log: str = None) -> object:
-        pass # lookup by string
+        pass # TODO: lookup by string
+
     def list_logs(self) -> list:
         return list(self._j.keys())
+
     def close(self):
         self._f.close()
 
 
-# Tests:
-"""my_json_reader = json_reader(file_path="../data/scrum_board_copy.json")
+"""# Tests:
+my_json_reader = json_reader(file_path="../data/scrum_board_copy.json")
 
-result = my_json_reader.delete(id=4)
-print(str(result))
+obj, log = my_json_reader.delete(id=4)
+print(str(obj))
 
 my_json_reader.close()"""
