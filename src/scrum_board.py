03519b511b71a99c94946a384e3f12020320b2d5
Caspar Popova <71401509+cjpopova@users.noreply.github.com>
2021-04-20 22:20:08 -0500

    Reset scrum_master & slack interface to main branch

--- src/scrum_board.py
+++ src/scrum_board.py
@@ -6,31 +6,20 @@ Scrum board API for simple interfacing with data
     td: to-do
 """
 import json
-import json_reader as jr
 
 SCRUM_BOARD = './data/scrum_board.json'
 
 class ScrumBoard:
     def __init__(self):
         # Can do this with other backlogs as well
-        """with open(SCRUM_BOARD, 'r') as pb:
-            self.pb = json.load(pb)"""
+        with open(SCRUM_BOARD, 'r') as pb:
+            self.pb = json.load(pb)
 
     def create_story(self):
         pass
 
-    def read_story(self, params: list): # id: int, log: str = None)
-        reader = jr.json_reader(file_path="data/scrum_board.json")  # Open reader
-        id = int(params[0])                                         # Get id, log from params
-        log = None
-        if len(params) > 1:
-            log = params[1]
-        obj, log_str = reader.read(id=id, log=log)             # Read from json
-        reader.close()                                         # Close json reader
-        if obj is None or log_str is None:
-            return "Story not found."
-        return "Reading story from "+log_str+": "+json.dumps(obj)
-
+    def read_story(self):
+        pass
     
     def update_story(self):
         pass
--- src/scrum_master.py
+++ src/scrum_master.py
@@ -30,14 +30,6 @@ class ScrumMaster:
         Need to make some assumptions about how users will communicate with the bot (at least pre-NLP)
         """
         self.text = text
-        l = text.split()                        # input string -> list of words
-        cmd = l[0]                              # Element 0 = command (eg read, update, etc)
-        if cmd not in list(ScrumMaster.commands.keys()):
-            self.text = "scrum_master: process_text expected first word to be command (create, read, update, or delete"
-            return
-        action = ScrumMaster.commands[l[0]]
-        self.text = action(l[1:len(l)])         # Element 1:len = parameters (eg ID, log, etc)
-        """ Using self.text for the return string - Caspar """
         pass
 
     def get_response(self):
