42ea8707ca09b319ce7ccf430badae7baf269434
Caspar Popova <71401509+cjpopova@users.noreply.github.com>
2021-04-20 22:21:54 -0500

    Untested stub code for reading story

--- src/scrum_board.py
+++ src/scrum_board.py
@@ -6,20 +6,30 @@ Scrum board API for simple interfacing with data
     td: to-do
 """
 import json
+import json_reader as jr
 
-SCRUM_BOARD = './data/scrum_board.json'
+SCRUM_BOARD = 'data/scrum_board.json'
 
 class ScrumBoard:
     def __init__(self):
         # Can do this with other backlogs as well
         with open(SCRUM_BOARD, 'r') as pb:
             self.pb = json.load(pb)
+        self.reader = jr.json_reader(file_path=SCRUM_BOARD)  # Open reader
 
     def create_story(self):
         pass
 
-    def read_story(self):
-        pass
+    def read_story(self, params: list): # [id: int, log: str = None]
+        id = int(params[0])  # Get id
+        log = None
+        if len(params) > 1:
+            log = params[1]  # Get user-specified log is possible
+        obj, log_str = self.reader.read(id=id, log=log)  # Read from json
+        if obj is None or log_str is None:
+            return "Story not found."
+        return "Reading story from "+log_str+": "+json.dumps(obj)
+
     
     def update_story(self):
         pass
