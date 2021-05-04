8289e3fa15e21a92df680dc97df8cf9e24375a05
Caspar Popova <71401509+cjpopova@users.noreply.github.com>
2021-05-04 15:11:30 -0500

    Fixed current sid for today's demo

--- src/scrum_master.py
+++ src/scrum_master.py
@@ -52,7 +52,7 @@ class ScrumMaster:
         self.current_sprint = 0
 
         # Next story id
-        self.sid = 1
+        self.sid = 11
 
         # Interface with JSON data
         self.scrum_board = ScrumBoard()
